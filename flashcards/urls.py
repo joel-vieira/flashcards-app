d3695dc24595270d1a5bd0e96b9b794784a8a955
Joel Vieira <joelvieira94@gmail.com>
2026-05-08 22:00:06 -0400

    [FEATURE] Add basic CSS

--- flashcards/urls.py
+++ flashcards/urls.py
@@ -1,3 +1,4 @@
+from django.views.generic.base import RedirectView
 from django.urls import path
 
 from . import views
@@ -5,6 +6,7 @@ from . import views
 app_name = 'flashcards'
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='dashboard/', permanent=False)),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('decks/', views.DecksIndexView.as_view(), name='decks'),
     path('decks/<int:deck_id>/', views.FlashcardsIndexView.as_view(), name='flashcards'),
